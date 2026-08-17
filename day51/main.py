a4f704fd8ced02b3b3e94a453fdd6fff962d6181
Ajay <ajaykh053@gmail.com>
2026-08-17 14:13:14 +0530

    runned day51

--- day51/main.py
+++ day51/main.py
@@ -6,6 +6,6 @@ Net_data = tweetbot.get_internet_speed()
 message = f"hey internet provider,  why is my iinternet speed is {Net_data[0].text}Down/{Net_data[1].text}Up When i pay for {DOWN_SPEED}Down/{UP_SPEED}Up"
 print(Net_data[0].text)
 print(Net_data[1].text)
-tweetbot.tweet_at_provider(message)
-# if float(Net_data[0].text)<DOWN_SPEED or float(Net_data[1].text)<UP_SPEED:
-#     tweetbot.tweet_at_provider(Message=message)
+# tweetbot.tweet_at_provider(message)
+if float(Net_data[0].text)<DOWN_SPEED or float(Net_data[1].text)<UP_SPEED:
+    tweetbot.tweet_at_provider(Message=message)
