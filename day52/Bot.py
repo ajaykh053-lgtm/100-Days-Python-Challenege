f3e873c76105dd7f0bc0381eac9c2e82907fb6e5
Ajay <ajaykh053@gmail.com>
2026-08-18 21:52:05 +0530

    modified day52

---
+++ day52/Bot.py
@@ -0,0 +1,56 @@
+import os
+from time import sleep
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+load_dotenv()
+SIMILAR_NAME = "chefsteps"
+class InstaFollower:
+    def __init__(self):
+        self.chrome_option = webdriver.ChromeOptions()
+        self.chrome_option.add_experimental_option("detach", True)
+        self.user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+        # include double -- for command line argument to Chrome.
+        self.chrome_option.add_argument(f"--user-data-dir={self.user_data_dir}")
+        self.driver = webdriver.Chrome(options=self.chrome_option)
+        self.wait = WebDriverWait(self.driver, 3)
+    def login(self):
+        self.driver.get(url=f"{os.environ['SHAREANAN']}/login")
+        Email_input = self.driver.find_element(by=By.NAME,value="username")
+        Email_input.send_keys(os.environ['EMAIL'])
+        sleep(1)
+        pass_input = self.driver.find_element(by=By.NAME,value="password")
+        pass_input.send_keys(os.environ['PASSWORD'])
+        sleep(1)
+        Login_btn = self.driver.find_element(by=By.CSS_SELECTOR,value=".naan-btn-primary")
+        Login_btn.click()
+        self.wait.until(ec.presence_of_all_elements_located((By.CLASS_NAME,"naan-popup-card")))
+        self.save_credentail = self.driver.find_element(by=By.CLASS_NAME,value="naan-popup-dismiss")
+        self.driver.execute_script("arguments[0].click();",self.save_credentail)
+        sleep(1)
+        self.wait.until(ec.presence_of_all_elements_located((By.CLASS_NAME,"naan-popup-overlay")))
+        self.dismiss_notification = self.driver.find_element(by=By.CLASS_NAME,value="naan-popup-dismiss")
+        self.driver.execute_script("arguments[0].click();",self.dismiss_notification)
+    def find_followers(self):
+        self.search = self.driver.find_element(by=By.CLASS_NAME,value="naan-rail-item")
+        self.driver.execute_script("arguments[0].click();",self.search)
+        sleep(2)
+        self.chefsteps = self.driver.find_element(by=By.XPATH,value="/html/body/aside/div[4]/a[1]")
+        self.driver.execute_script("arguments[0].click();",self.chefsteps)
+        self.wait.until(ec.presence_of_all_elements_located((By.CLASS_NAME,"naan-center")))
+        # self.driver.get(url=f"{os.environ['SHAREANAN']}/u/{SIMILAR_NAME}/followers")
+        self.follower = self.driver.find_element(by=By.CSS_SELECTOR,value=".naan-followers-link")
+        self.driver.execute_script("arguments[0].click();",self.follower)
+        sleep(2)
+        self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;", self.follower)
+    def follow(self):
+        self.follow_btn = self.driver.find_elements(by=By.CSS_SELECTOR,value=".naan-follow-btn")
+        for btn in reversed(self.follow_btn):
+            if 'is-following' in btn.get_attribute(name="class") :#type: ignore
+                pass
+            else:
+                btn.click()
+                print(f"Followed the {self.driver.find_element(by=By.XPATH,value="..").text}")
+                sleep(1)
