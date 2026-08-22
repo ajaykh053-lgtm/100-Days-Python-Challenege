24e2da14f7bef8ae820eb1ff1c409f2c7ef353c3
Ajay <ajaykh053@gmail.com>
2026-08-22 14:00:46 +0530

    day55 done and working

---
+++ day55/higherlower/server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from random import randint
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return (
+        "<h1>Guess a number between 0 and 9</h1>"
+        "<h2> write in url after the / </h2>"
+        '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGJnOGVqdTZld2VkdzJhaHI2ZWwyMmN1ZXFxZ2d4c3h1YTZhaWxwbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RVCJ3vwebUGDpoy7Tm/giphy.gif" y>'
+    )
+
+
+@app.route("/<int:userguess>")
+def user_number(userguess):
+    number = randint(0, 9999)
+    if userguess < number:
+        return (
+            "<h3>Fhaaaaaa you suck its too lowwwwww !</h3"
+            "<h3>Guess again ?</h3>"
+            '<img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcXN4MjM0cmZwcnVnb2RjZHZ6ZzE3cGhudDRhZjJ5d3IzbnNtajJ2bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Iy1PWGPb7Hz3AI4e3T/giphy.gif" y>'
+        )
+    elif userguess > number:
+        return (
+            "<h3>Fhaaaaaa you suck its too highhh !</h3"
+            "<h3>Guess again ?</h3>"
+            '<img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzViZGg5dWxraDd2MXJrOWVvN3ZqNW1nNzE0ZTQ1YXEycTY3ZTJybCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0SVAVxeJsnJ1WRMIPX/giphy.gif" >'
+        )
+    else:
+        return (
+            "<h3>All the best for next time. </h3"
+            "<h3>Guess again ?</h3>"
+            '<img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2k5bzNoeHB1N3UzdDBqZ3l2Y3V0eGNuNXJlNmFyNzk0YnRkdHJlYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YSD04aQmVadOQen7rH/giphy.gif" >'
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- day55/main.py
+++ day55/main.py
@@ -1,12 +1,64 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World'
+# app = Flask(__name__)
 
 
-if __name__=="__main__":
-    app.run()
+# @app.route("/")
+# def hello_world():
+#     return (
+#         '<h1 style="text-align: center">Hello, World</h1>'
+#         '<div style="display:flex" ><p>Why are you gay</p> <br> <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMW4waXhvNDYwNzl2eWphMXNqcnIzY2VsOWw5dW42OWU5YmE3OGxueiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gb6TyakSW7F3mKx6VT/giphy.gif" style="width = 200 align-item:flex-start" > <p>Ur Gay</p> <br> <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMTZxcDI1bXBsdDhoaTVzYXpmZTV0MnFiOWFjN2FteGxqN3d0b2NxaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gbwNUZEPU58BscyIqO/giphy.gif" style="width = 200 align-item:flex-end"></div>'
+#     )
 
+
+# def make_bold(fun):
+#     def wrapper():
+#         return "<b>" + fun() + "</b>"
+
+#     return wrapper
+
+
+# def make_emphasis(fun):
+#     def wrapper():
+#         return "<em>" + fun() + "</em>"
+
+#     return wrapper
+
+
+# def make_underlined(fun):
+#     def wrapper():
+#         return "<u>" + fun() + "</u>"
+
+#     return wrapper
+
+
+# @app.route("/bye")
+# @make_bold
+# @make_emphasis
+# @make_underlined
+# def bye():
+#     return "Bye"
+
+
+# @app.route("/username/<name>/<int:number>")
+# def greet(name, number):
+#     return f"Hello {name} you are {number} year old!"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+#Advance Decorators
+# TODO: Create the logging_decorator() function 👇
+def logging_decorator(fun):
+    def wrapper(*args):
+        print(f'You called a_function{(args)}\nIt returned {fun(*args)}')
+    return wrapper
+
+# TODO: Use the decorator 👇
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+    
+a_function(4,5,6)
