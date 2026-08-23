e3478deaaa7bd9774a1f431cb7708447eb333bf6
Ajay <ajaykh053@gmail.com>
2026-08-23 17:00:34 +0530

    Add initial styles and layout for "Identity" template by HTML5 UP
    - Created SASS files for IE8 and IE9 compatibility.
    - Added layout styles for footer, main content, and wrapper.
    - Implemented utility functions and mixins for consistent styling.
    - Defined global variables for sizes, fonts, and colors.
    - Included main SASS file to compile all styles.
    - Added noscript styles for better accessibility.
    - Included images for avatar and background.
    - Created HTML structure for the template with responsive design.

--- day56/main.py
+++ day56/main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route("/")
 def inedx():
     return render_template("index.html")
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+# By typing this in console fo chrome we can direct edit website from the chrome
+# document.body.contentEditable=true
