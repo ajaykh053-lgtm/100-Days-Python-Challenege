9fa104e5aa51eb4a84cf90cdf09fc0f9d3a118fc
Ajay <ajaykh053@gmail.com>
2026-08-27 22:19:01 +0530

    main modified

--- day59/main.py
+++ day59/main.py
@@ -5,3 +5,16 @@ import seaborn
 import matplotlib
 import numpy
 from cv2 import FaceRecognizerSF
+from flask import from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000,debug=True)
