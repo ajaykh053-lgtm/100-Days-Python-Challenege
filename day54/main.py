4297a29da0912f6e7e66f21523198f9306dc389a
Ajay <ajaykh053@gmail.com>
2026-09-04 23:54:17 +0530

    Ready to post

--- day54/main.py
+++ day54/main.py
@@ -1,4 +1,4 @@
-# Functions can have inputs
+# # Functions can have inputs
 # def add(n1, n2):
 #     return n1 + n2
 
@@ -13,25 +13,30 @@
 
 # def divide(n1, n2):
 #     return n1 / n2
-# Function can have other function as input
-# def calculte(cal_func,n1,n2):
-#     return cal_func(n1,n2)
-# result = calculte(multiply,2,3)
 
 
-# Nested function
-def outer_function():
-    print("I'm outer")
-
-    def nested_fintion():
-        print("I'm inner")
-
-    return nested_fintion
+# # Function can have other function as input
+# def calculte(cal_func, n1, n2):
+#     return cal_func(n1, n2)
 
 
-# Functions can be returned from other function
-inner_function = outer_function()
-inner_function()
+# result = calculte(multiply, 2, 3)
+# print(result)
+
+
+# # Nested function
+# def outer_function():
+#     print("I'm outer")
+
+#     def nested_fintion():
+#         print("I'm inner")
+
+#     return nested_fintion
+
+
+# # Functions can be returned from other function
+# inner_function = outer_function()
+# inner_function()
 
 
 # Python Decorator
@@ -39,10 +44,18 @@ inner_function()
 # and wrap that functio in wrapper function and returns the wrapper function
 def decorator_finction(function):
     def wrapper_function():
+        print(
+            "Adding Additional Fuctionalities to the exsisting function using decorator"
+        )
         function()
+        print("Ajay")
 
     return wrapper_function
 
 
+@decorator_finction
 def say_hi():
     print("hi")
+
+
+say_hi()
