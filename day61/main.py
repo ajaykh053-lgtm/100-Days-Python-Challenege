e03deef3c669e308939567195f018a639d5fec3c
Ajay <ajaykh053@gmail.com>
2026-09-06 01:53:11 +0530

    day61 done

--- day61/main.py
+++ day61/main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap4
 
 ##Creating forms with the help of the flask
 from flask_wtf import FlaskForm
 from flask_wtf.form import _Auto
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
+
+app = Flask(__name__)
+bootstrap = Bootstrap4(app=app)
+app.secret_key = "CreatingFormform-the-flask"
 
 
 def my_length_check(form, password):
@@ -18,14 +23,11 @@ def my_length_check(form, password):
 #         if "." not in email.data:
 #             raise ValidationError("Invalid email")
 
+
 class LoginForm(FlaskForm):
     email = StringField("email", validators=[DataRequired(), Email()])
     password = PasswordField("password", validators=[DataRequired(), my_length_check])
     submit = SubmitField("Log In")
-
-
-app = Flask(__name__)
-app.secret_key = "CreatingForm"
 
 
 @app.route("/")
@@ -36,11 +38,15 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    print(login_form.validate_on_submit())
     if login_form.validate_on_submit():
-        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
-                return render_template("success.html")
+        if (
+            login_form.email.data == "admin@email.com"
+            and login_form.password.data == "12345678"
+        ):
+            return render_template("success.html")
         else:
-                return render_template("denied.html")
+            return render_template("denied.html")
     return render_template("login.html", form=login_form)
 
 
