a81d564a68b0f1504a2602dcd634a56cc22f6ce0
Ajay <ajaykh053@gmail.com>
2026-09-07 00:35:28 +0530

    main added

--- day63/main.py
+++ day63/main.py
@@ -7,12 +7,12 @@ all_books = []
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/add",methods=['POST'])
+@app.route("/add", methods=["GET","POST"])
 def add():
-    return render_template('add.html')
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
