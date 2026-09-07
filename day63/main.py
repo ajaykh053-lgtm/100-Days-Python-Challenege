1d111f5b9f6770f424d10843e3968f3e235eb087
Ajay <ajaykh053@gmail.com>
2026-09-07 17:18:39 +0530

    ..

--- day63/main.py
+++ day63/main.py
@@ -2,15 +2,33 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-all_books = []
+all_books = [
+    {
+        "title": "Harry Potter",
+        "author": "J. K. Rowling",
+        "rating": 9,
+    },
+    {
+        "title": "Tail of two cities",
+        "author": "Chalres Dickens",
+        "rating": 7,
+    }
+]
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        book_dict = {
+            'title' : f"{request.form['Bookname']}",
+            'author' : f"{request.form['BookAuthor']}",
+            'rating' : f"{request.form['Rating']}"
+        }
+        all_books.append(book_dict)
+    return render_template("index.html",all_books=all_books)
 
 
-@app.route("/add", methods=["GET","POST"])
+@app.route("/add")
 def add():
     return render_template("add.html")
 
