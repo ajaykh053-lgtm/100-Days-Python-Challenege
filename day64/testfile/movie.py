07751cde10b5fef65cdc599b47639dce730b0377
Ajay <ajaykh053@gmail.com>
2026-09-10 00:22:34 +0530

    Update index.html and movie.py: Modify card image source, update card title and text, and enhance movie API request handling with environment variables

--- day64/testfile/movie.py
+++ day64/testfile/movie.py
@@ -1,12 +1,23 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-url = "https://api.themoviedb.org/3/movie/day?language=en-US"
+load_dotenv()
+import requests
 
+url = "https://api.themoviedb.org/3/search/movie?include_adult=true&language=en-US&page=1"
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDBlZjFiNGUwMTkyZjA2YzI4ODMyZTZiZWM2ZjQ3YyIsIm5iZiI6MTc4ODg3NTQzNS40MjcsInN1YiI6IjZhYTAxMmFiYTNiZWNiYmZjNGM3NjQxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aJC8UXuev_dSzScRwW8FDF4ogRNkqTB3-IIv8gGQwko"
 }
+response = requests.get(url,params={"api_key":os.environ['API_KEY'],"query":"The Super Mario Galaxy Movie"}, headers=headers)
 
-response = requests.get(url, headers=headers)
-with open(file='json.txt',mode='w') as file:
+# print(response.text)
+# headers = {
+#     "accept": "application/json",
+#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDBlZjFiNGUwMTkyZjA2YzI4ODMyZTZiZWM2ZjQ3YyIsIm5iZiI6MTc4ODg3NTQzNS40MjcsInN1YiI6IjZhYTAxMmFiYTNiZWNiYmZjNGM3NjQxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aJC8UXuev_dSzScRwW8FDF4ogRNkqTB3-IIv8gGQwko",
+# }
+# params = {"api_key": f"{os.environ['API_KEY']}","query": "movie_title"}
+# response = requests.get(os.environ["MOVIE_ENDPOINT"], params=params, headers=headers)
+with open(file="result.json", mode="w") as file:
     file.write(response.text)
