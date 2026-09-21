d79296f065a97c39c0e1d2a75165f475121dfd90
Ajay <ajaykh053@gmail.com>
2026-09-22 00:30:20 +0530

    day69 ready to post

--- day69/main.py
+++ day69/main.py
@@ -60,7 +60,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///postsprefinal.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///postsday69.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -243,8 +243,12 @@ def edit_post(post_id):
 @admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
+    result = db.session.execute(db.select(Comment).where(Comment.post_id == post_id))
+    comments_to_delete = result.scalars().all()
     db.session.delete(post_to_delete)
-    db.session.commit()
+    for comments in comments_to_delete:
+        db.session.delete(comments)
+        db.session.commit()
     return redirect(url_for("get_all_posts"))
 
 
